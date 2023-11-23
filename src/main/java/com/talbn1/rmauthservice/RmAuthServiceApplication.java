e4459cf18b4832e9b47e4a6372fccf121cfb2e12
Tal <talbn1@gmail.com>
2023-11-23 18:23:23 +0200

    Initial commit

---
+++ src/main/java/com/talbn1/rmauthservice/RmAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.talbn1.rmauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmAuthServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RmAuthServiceApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/talbn1/rmauthservice/RmAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talbn1.rmauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmAuthServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
